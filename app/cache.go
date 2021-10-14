da1922dbcabe77ad501773cbe03941f01e68447c
Thunnini <yunjh1994@gmail.com>
2021-10-14 17:23:18 +0900

    Increase the gov tally caching lifetime

--- app/cache.go
+++ app/cache.go
@@ -10,6 +10,6 @@ var (
 	// Querying gov tally calculates the all voting of the validators and delegators.
 	// It can take a time 20sec ~ 30sec.
 	// Immediate response is less important compared to how long this query takes.
-	// Therefore, to reduce resource usage, cache for 5 minutes.
-	queryGovTallyCache = cache.New(5*time.Minute, 10*time.Minute)
+	// Therefore, to reduce resource usage, cache for 30 minutes.
+	queryGovTallyCache = cache.New(30*time.Minute, 30*time.Minute)
 )
